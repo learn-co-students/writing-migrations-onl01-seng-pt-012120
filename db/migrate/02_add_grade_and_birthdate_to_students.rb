7eb2001e4e9e255c6504bbbc6d340605da266061
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-03-31 16:10:58 +0000

    Done.

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,8 +1,9 @@
 
-class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
 
   def change
-    add_column :students, :grade, :integer, :birthdate, :string
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
   end
 
 end
